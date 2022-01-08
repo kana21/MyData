78598d81d19646f12d5a68c08ec5995082ff4a44
kana <jcanat@outlook.com>
2022-01-08 17:44:48 +0100

    Network permission and status

--- app/src/main/java/com/coolweb/android/mydata/data/BookRepository.kt
+++ app/src/main/java/com/coolweb/android/mydata/data/BookRepository.kt
@@ -1,7 +1,11 @@
 package com.coolweb.android.mydata.data
 
 import android.app.Application
+import android.content.Context
+import android.net.ConnectivityManager
+import android.util.Log
 import androidx.lifecycle.MutableLiveData
+import com.coolweb.android.mydata.LOG_TAG
 import com.coolweb.android.mydata.utilities.FileHelper
 import com.squareup.moshi.JsonAdapter
 import com.squareup.moshi.Moshi
@@ -19,6 +23,7 @@ class BookRepository(val app: Application) {
 
     init {
         getBookData2()
+        Log.i(LOG_TAG, "Network available: ${networkAvailable()}")
     }
 
     fun getBookData(): List<Book> {
@@ -37,5 +42,13 @@ class BookRepository(val app: Application) {
             .build()
         val adapter: JsonAdapter<List<Book>> = moshi.adapter(listType)
         bookData.value = adapter.fromJson(text) ?: emptyList()
+    }
+
+    @Suppress("DEPRECIATION")
+    private fun networkAvailable(): Boolean {
+        val connectivityManager = app.getSystemService(Context.CONNECTIVITY_SERVICE)
+        as ConnectivityManager
+        val networkInfo = connectivityManager.activeNetworkInfo
+        return networkInfo?.isConnectedOrConnecting ?: false
     }
 }
